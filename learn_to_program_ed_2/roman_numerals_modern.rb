b1208242bcb336f5d49272f347eba36f765b86f9
Jeff Fichtner <jff2501@gmail.com>
2016-08-10 17:44:39 -0700

    Add working program

--- learn_to_program_ed_2/roman_numerals_modern.rb
+++ learn_to_program_ed_2/roman_numerals_modern.rb
@@ -19,25 +19,43 @@ while true
 		break
 	else
 
-		while true
+		while true 
 			if number >= 1000
 				number = subtract(number, 1000)
 				roman_numeral = add_to_numeral("M", roman_numeral)
+			elsif number >= 900
+				number = subtract(number, 900)
+				roman_numeral = add_to_numeral("CM", roman_numeral)
 			elsif number >= 500
 				number = subtract(number, 500)
 				roman_numeral = add_to_numeral("D", roman_numeral)
+			elsif number >= 400
+				number = subtract(number, 400)
+				roman_numeral = add_to_numeral("CD", roman_numeral)
 			elsif number >= 100
 				number = subtract(number, 100)
 				roman_numeral = add_to_numeral("C", roman_numeral)
+			elsif number >= 90
+				number = subtract(number, 90)
+				roman_numeral = add_to_numeral("XC", roman_numeral)
 			elsif number >= 50
 				number = subtract(number, 50)
 				roman_numeral = add_to_numeral("L", roman_numeral)
+			elsif number >= 40
+				number = subtract(number, 40)
+				roman_numeral = add_to_numeral("XL", roman_numeral)
 			elsif number >= 10
 				number = subtract(number, 10)
 				roman_numeral = add_to_numeral("X", roman_numeral)
+			elsif number >= 9
+				number = subtract(number, 9)
+				roman_numeral = add_to_numeral("IX", roman_numeral)
 			elsif number >= 5
 				number = subtract(number, 5)
 				roman_numeral = add_to_numeral("V", roman_numeral)
+			elsif number >= 4
+				number = subtract(number, 4)
+				roman_numeral = add_to_numeral("IV", roman_numeral)
 			elsif number >= 1
 				number = subtract(number, 1)
 				roman_numeral = add_to_numeral("I", roman_numeral)
@@ -51,3 +69,13 @@ while true
 	end
 
 end
+
+
+# >3 added together
+
+# IV	4 = 5 - 1
+# IX	9 = 10 - 1
+# XL	40 = 50 - 10
+# XC	90 = 100 - 10
+# CD	400 = 500 - 100
+# CM	900 = 100 - 100
