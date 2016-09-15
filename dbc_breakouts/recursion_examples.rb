01909ce4c7c8dab7c51484df14129cd5a4e9f93f
Jeff Fichtner <jff2501@gmail.com>
2016-09-15 15:28:09 -0500

    Fix iterative solutions

--- dbc_breakouts/recursion_examples.rb
+++ dbc_breakouts/recursion_examples.rb
@@ -1,49 +1,69 @@
 # reverse iteration
-def reverse_iteratively word
-  reversed_word = ""
-  word.chars.each { |char| reversed_word.prepend(char) }
-  reversed_word
+def reverse_iteratively(word)
+  reversed_word = []
+	last_index = word.length - 1
+
+  word.chars.each_with_index do |char, i| 
+  	reversed_word[last_index - i] = char
+  end
+
+  reversed_word.join
 end
 
 # palindrome iteration
-def palindrome_iteratively? word
-  if word == reverse_iteratively(word)
-    return true
-  end
-  false
+def palindrome_iteratively?(word)
+	last_index = word.length - 1
+
+	last_index.times do |i|
+		if (word[i] != word[last_index - i] || i > last_index)
+			return false
+		end
+	end
+
+	true
 end
 
 # reverse recursion
-def reverse_recursively word
+def reverse_recursively(word)
 	return word if word.length <= 1
+
 	reversed_word = ""
-	reversed_word << word[-1] + reverse_recursively(word[0..-2])
+	last_letter = word[-1]
+	rest_of_word = word[0..-2]
+
+	reversed_word << last_letter + reverse_recursively(rest_of_word)
 end
 
 # palindrome recursion
-def palindrome_recursively? word
+def palindrome_recursively?(word)
 	return true if word.length <= 1
-	word[0] == word[-1] && palindrome_recursively?(word[1..-2])
+
+	first_letter = word[0]
+	last_letter = word[-1]
+	rest_of_word = word[1..-2]
+
+	first_letter == last_letter && palindrome_recursively?(rest_of_word)
 end
 
 
-non_palindrome = 'reverse'
-palindrome = 'racecar'
 
-puts "non_palindrome: reverse; palindrome: racecar\n"
+# non_palindrome = 'reverse'
+# palindrome = 'racecar'
 
-puts "\nReverse iteratively:"
-p reverse_iteratively(non_palindrome)
-p reverse_iteratively(palindrome)
+# puts "non_palindrome: reverse; palindrome: racecar\n"
 
-puts "\nPalindrome? iteratively:"
-p palindrome_iteratively?(non_palindrome)
-p palindrome_iteratively?(palindrome)
+# puts "\nReverse iteratively:"
+# p reverse_iteratively(non_palindrome)
+# p reverse_iteratively(palindrome)
 
-puts "\nReverse recursively:"
-p reverse_recursively(non_palindrome)
-p reverse_recursively(palindrome)
+# puts "\nPalindrome? iteratively:"
+# p palindrome_iteratively?(non_palindrome)
+# p palindrome_iteratively?(palindrome)
 
-puts "\nPalindrome? recursively:"
-p palindrome_recursively?(non_palindrome)
-p palindrome_recursively?(palindrome)
+# puts "\nReverse recursively:"
+# p reverse_recursively(non_palindrome)
+# p reverse_recursively(palindrome)
+
+# puts "\nPalindrome? recursively:"
+# p palindrome_recursively?(non_palindrome)
+# p palindrome_recursively?(palindrome)
